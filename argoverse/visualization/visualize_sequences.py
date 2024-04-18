0c93e2e9e58d6f0438b4c261f34f99aa4334c24c
aurandt <aurandt@iastate.edu>
2024-04-18 15:42:08 -0400

    Adding Argoverse API directly into repo

---
+++ argoverse/visualization/visualize_sequences.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+
+"""A simple python script template."""
+from collections import defaultdict
+from typing import Dict, List, Optional
+
+import matplotlib.lines as mlines
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.interpolate as interp
+
+from argoverse.map_representation.map_api import ArgoverseMap
+
+_ZORDER = {"AGENT": 15, "AV": 10, "OTHERS": 5}
+
+
+def interpolate_polyline(polyline: np.ndarray, num_points: int) -> np.ndarray:
+    duplicates = []
+    for i in range(1, len(polyline)):
+        if np.allclose(polyline[i], polyline[i - 1]):
+            duplicates.append(i)
+    if polyline.shape[0] - len(duplicates) < 4:
+        return polyline
+    if duplicates:
+        polyline = np.delete(polyline, duplicates, axis=0)
+    tck, u = interp.splprep(polyline.T, s=0)
+    u = np.linspace(0.0, 1.0, num_points)
+    return np.column_stack(interp.splev(u, tck))
+
+
+def viz_sequence(
+    df: pd.DataFrame,
+    lane_centerlines: Optional[List[np.ndarray]] = None,
+    show: bool = True,
+    smoothen: bool = False,
+):
+
+    x_agent, y_agent, x_av, y_av = 0.0, 0.0, 0.0, 0.0
+    # Seq data
+    city_name = df["CITY_NAME"].values[0]
+
+    if lane_centerlines is None:
+        # Get API for Argo Dataset map
+        avm = ArgoverseMap()
+        seq_lane_props = avm.city_lane_centerlines_dict[city_name]
+
+    plt.figure(0, figsize=(8, 7))
+
+    x_min = min(df["X"])
+    x_max = max(df["X"])
+    y_min = min(df["Y"])
+    y_max = max(df["Y"])
+
+    if lane_centerlines is None:
+
+        plt.xlim(x_min, x_max)
+        plt.ylim(y_min, y_max)
+
+        lane_centerlines = []
+        # Get lane centerlines which lie within the range of trajectories
+        for lane_id, lane_props in seq_lane_props.items():
+
+            lane_cl = lane_props.centerline
+
+            if (
+                np.min(lane_cl[:, 0]) < x_max
+                and np.min(lane_cl[:, 1]) < y_max
+                and np.max(lane_cl[:, 0]) > x_min
+                and np.max(lane_cl[:, 1]) > y_min
+            ):
+                lane_centerlines.append(lane_cl)
+
+    for lane_cl in lane_centerlines:
+        plt.plot(
+            lane_cl[:, 0],
+            lane_cl[:, 1],
+            "--",
+            color="grey",
+            alpha=1,
+            linewidth=1,
+            zorder=0,
+        )
+    frames = df.groupby("TRACK_ID")
+
+    plt.xlabel("Map X")
+    plt.ylabel("Map Y")
+
+    color_dict = {"AGENT": "#d31f11", "OTHERS": "#d3e8ef", "AV": "#d31f11"}
+    object_type_tracker: Dict[int, int] = defaultdict(int)
+
+    # Plot all the tracks up till current frame
+    for group_name, group_data in frames:
+        object_type = group_data["OBJECT_TYPE"].values[0]
+        if object_type == "OTHERS":
+            continue
+
+        cor_x = group_data["X"].values
+        cor_y = group_data["Y"].values
+
+        if smoothen:
+            polyline = np.column_stack((cor_x, cor_y))
+            num_points = cor_x.shape[0] * 3
+            smooth_polyline = interpolate_polyline(polyline, num_points)
+            cor_x = smooth_polyline[:, 0]
+            cor_y = smooth_polyline[:, 1]
+
+        if(object_type == "AGENT"):
+            plt.plot(
+                cor_x[:20],
+                cor_y[:20],
+                "-",
+                color=color_dict[object_type],
+                label="Observed Trajectory",
+                alpha=1,
+                linewidth=2,
+                zorder=_ZORDER[object_type],
+            )
+        else:
+            plt.plot(
+                cor_x[:20],
+                cor_y[:20],
+                "-",
+                color=color_dict[object_type],
+                alpha=1,
+                linewidth=2,
+                zorder=_ZORDER[object_type],
+            )
+
+        if len(cor_x) == 50:
+            final_x = cor_x[19]
+            final_y = cor_y[19]
+            if object_type == "AGENT":
+                x_agent = final_x
+                y_agent = final_y
+            elif object_type == "AV":
+                x_av = final_x
+                y_av = final_y
+        else:
+            final_x = cor_x[-1]
+            final_y = cor_y[-1]
+
+        if object_type == "AGENT":
+            marker_type = "o"
+            marker_size = 7
+        elif object_type == "OTHERS":
+            marker_type = "o"
+            marker_size = 7
+        elif object_type == "AV":
+            marker_type = "o"
+            marker_size = 7
+
+        if object_type == "AGENT":
+            plt.plot(
+                final_x,
+                final_y,
+                marker_type,
+                color=color_dict[object_type],
+                label="Current Vehicle Location",
+                alpha=1,
+                markersize=marker_size,
+                zorder=_ZORDER[object_type],
+            )
+        else:
+                plt.plot(
+                final_x,
+                final_y,
+                marker_type,
+                color=color_dict[object_type],
+                alpha=1,
+                markersize=marker_size,
+                zorder=_ZORDER[object_type],
+            )
+
+        object_type_tracker[object_type] += 1
+
+    red_star = mlines.Line2D([], [], color="red", marker="*", linestyle="None", markersize=7, label="Agent")
+    green_circle = mlines.Line2D(
+        [],
+        [],
+        color="green",
+        marker="o",
+        linestyle="None",
+        markersize=7,
+        label="Others",
+    )
+    black_triangle = mlines.Line2D([], [], color="black", marker="^", linestyle="None", markersize=7, label="AV")
+
+    #plt.axis("off")
+    if show:
+        plt.show()
+    return x_agent, y_agent, x_av, y_av
